1f8f3eb2dd8be3f454d261f024b0526e3aff1739
Leo Soares <leojrfs@gmail.com>
2017-01-16 15:44:08 +0000

    minor temporary tweaks

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 BUILD_DEPS="debootstrap squashfs-tools"
-PACKAGES="ifupdown,ifupdown-extra,isc-dhcp-client,openssh-server,less,nano,picocom,htop"
+PACKAGES="ifupdown,ifupdown-extra,isc-dhcp-client,openssh-server,less,nano,picocom,htop,live-boot,live-boot-initramfs-tools,linux-image-586"
+MIRROR="http://ftp.pt.debian.org/debian"
+#MIRROR="http://httpredir.debian.org/debian"
 
 set -e
 
@@ -11,7 +13,7 @@ mkdir "${WORK_DIR}"
 apt-get update
 apt-get install -y ${BUILD_DEPS}
 
-debootstrap --variant=minbase --arch=i386 --include=$PACKAGES jessie "${WORK_DIR}" http://httpredir.debian.org/debian
+debootstrap --verbose --variant=minbase --arch=i386 --include=$PACKAGES jessie "${WORK_DIR}" $MIRROR
 
 ## Post debootstrap customization
 # Clean up file with misleading information from host
@@ -28,6 +30,20 @@ auto eth0
 iface eth0 inet dhcp
 EOF
 
+# Hosts files
+cat >>"${WORK_DIR}/etc/hosts" <<'EOF'
+127.0.0.1 localhost
+127.0.1.1 live-boot
+
+# The following lines are desirable for IPv6 capable hosts
+::1     ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+EOF
+
 # Implement insecurity
 chroot "${WORK_DIR}" passwd -d root # remove password on root account
 sed -i 's/pam_unix.so nullok_secure/pam_unix.so nullok/' "${WORK_DIR}/etc/pam.d/common-auth"
@@ -38,9 +54,12 @@ sed -i 's/PermitEmptyPasswords no/PermitEmptyPasswords yes/' "${WORK_DIR}/etc/ss
 rm -rf "${WORK_DIR}"/var/cache/apt/*
 rm -rf "${WORK_DIR}"/tmp/*
 
-# Build the root filesystem image, and extract the accompanying kernel and initramfs
-mksquashfs "${WORK_DIR}" sqashfs.new -noappend; mv sqashfs.new /tftp/filesystem.squashfs
 
-chroot "${WORK_DIR}" apt-get install -y linux-image-586 live-boot live-boot-initramfs-tools
+# Extract the accompanying kernel and initramfs
 cp -p "${WORK_DIR}/boot"/vmlinuz-* vmlinuz.new; mv vmlinuz.new /tftp/vmlinuz
 cp -p "${WORK_DIR}/boot"/initrd.img-* initrd.new; mv initrd.new /tftp/initrd.img
+
+
+# Remove the boot directory, and build the root filesystem image
+rm -rf "${WORK_DIR}/boot"/*
+mksquashfs "${WORK_DIR}" sqashfs.new -noappend; mv sqashfs.new /tftp/filesystem.squashfs
